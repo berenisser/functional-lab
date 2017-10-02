749b6b248fc82a0f46c4152657984fb93c20d572
berenisser <berenisser@gmail.com>
2017-10-01 22:30:57 -0300

    se regreso a iteracion en vez de high order function map para que pasen todos los tests

--- 01-state/03-parse-items/parseItems.js
+++ 01-state/03-parse-items/parseItems.js
@@ -2,7 +2,10 @@ const parseItems = (items) => {
    const cosas = [...items];  
   //En teoria la funcion map con el parametro de str deberia solucionar el 
   //conflicto que retorna [1, NaN, NaN] en vez de un arreglo de integers
-  cosas.map(str => parseInt(str) );
+  //cosas.map(str => parseInt(str) );
+  for (let i = 0; i < cosas.length; i++) {
+ 		cosas[i] = parseInt(cosas[i]);
+ 	}
   return cosas.sort();
 };
 
